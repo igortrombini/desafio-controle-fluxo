f4c61952d7221714d230ed3555ae37a36ff989f4
igortrombini <igorts20@gmail.com>
2023-08-24 14:53:30 -0300

    desafio controle fluxo - contador

---
+++ src/Contador.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+    
+        
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {            // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        
+        if (parametroUm >= parametroDois)
+            throw new ParametrosInvalidosException("O Segundo parametro deve ser maior que o primeiro");
+        int contagem = parametroDois - parametroUm + 1;
+        
+        for (int i = 0; i < contagem; i++) {                    // realizar o for para imprimir os números com base na variável contagem
+            System.out.println("Imprimindo o número " + i);
+
+        }
+    }
+}
